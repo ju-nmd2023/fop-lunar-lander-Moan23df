fc6ff5889183e0b24056c6b2f1d9721e3b0d712b
Moan23df <moan23df@student.ju.se>
2024-02-18 14:13:00 +0100

    update

--- Lunar-lander.js
+++ Lunar-lander.js
@@ -5,9 +5,34 @@ let x2 = 500;
 let velocity = 1;
 const accelaration = 0.1;
 const speed = 5;
+let gameState = "start";
 
 function setup() {
   createCanvas(800, 800);
+}
+
+function draw() {
+  if (gameState === "start") {
+    startScreen();
+  } else if (gameState === "game") {
+    gameScreen();
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (gameState === "start") {
+      gameState = "game";
+    }
+  }
+}
+
+function startScreen() {
+  background(255, 255, 255);
+  textSize(40);
+  fill(0, 0, 0);
+  textAlign(CENTER);
+  text("Press Space To Start The Game", width / 2, height / 2);
 }
 
 function ground() {
@@ -49,6 +74,10 @@ function rocket() {
   ellipse(rocketx, rockety + 20, 7);
   ellipse(rocketx, rockety + 40, 7);
   ellipse(rocketx, rockety + 60, 7);
+
+  if (velocity > 0) {
+    flames();
+  }
 }
 
 function meteoroid(x, y) {
@@ -74,7 +103,41 @@ function meteoroid(x, y) {
   ellipse(210, 160, 15);
 }
 
-function draw() {
+function flames() {
+  noStroke();
+  fill(255, 165, 0);
+  let flameY = rockety + 180;
+  triangle(rocketx - 30, flameY, rocketx, flameY + 80, rocketx + 40, flameY);
+  triangle(rocketx - 60, flameY, rocketx - 30, flameY + 60, rocketx, flameY);
+  triangle(
+    rocketx + 10,
+    flameY,
+    rocketx + 40,
+    flameY + 60,
+    rocketx + 70,
+    flameY
+  );
+  fill(252, 230, 140);
+  triangle(rocketx - 20, flameY, rocketx, flameY + 50, rocketx + 30, flameY);
+  triangle(
+    rocketx - 50,
+    flameY,
+    rocketx - 30,
+    flameY + 40,
+    rocketx - 10,
+    flameY
+  );
+  triangle(
+    rocketx + 15,
+    flameY,
+    rocketx + 40,
+    flameY + 40,
+    rocketx + 60,
+    flameY
+  );
+}
+
+function gameScreen() {
   clear();
   ground();
   rocket();
